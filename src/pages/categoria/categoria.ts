569ec69f8888d744887d97b825df1a57f7503b3a
viktor-lima <joaolimak10@outlook.com>
2022-04-12 02:07:53 -0300

    Listando Categorias

--- src/pages/categoria/categoria.ts
+++ src/pages/categoria/categoria.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { CategoriaService } from '../../services/domain/categoria.service';
 
 /**
  * Generated class for the CategoriaPage page.
@@ -15,11 +16,23 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CategoriaPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public categoriaService: CategoriaService
+    ){
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad CategoriaPage');
+    this.categoriaService.findAll()
+      .subscribe(response => {
+        console.log(response);
+      },
+      error =>{
+        console.log(error);
+        
+      });
+    
   }
 
 }
