179696f25cb66b960a080dc2d226f273ea2b72b2
viktor-lima <joaolimak10@outlook.com>
2022-04-13 20:51:39 -0300

    Trantando erros 403

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -1,9 +1,15 @@
 import { Injectable } from "@angular/core";
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { Observable } from "rxjs";
+import { StorageService } from "../services/storage.service";
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
+
+    constructor(public storage : StorageService){
+
+    }
+
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
        
         return next.handle(req)
@@ -18,10 +24,22 @@ export class ErrorInterceptor implements HttpInterceptor {
 
                 console.log("error detectado ")
                 console.log(errorObj);
+
+                switch (errorObj.status) {
+                    case 403:
+                        this.handle403
+                        break;
+                    default:
+                        break;
+                }
                 
 
               return Observable.throw(errorObj);
             }) as any;
+    }
+
+    handle403(){
+        this.storage.setLocalUser(null);
     }
 }
 
--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -30,7 +30,13 @@ export class ProfilePage {
           //buscar imagem
           this.getImageIfExists();
         },
-        error =>{});
+        error =>{
+          if(error.status == 403)
+            this.navCtrl.setRoot('HomePage');
+        });
+    }
+    else{
+      this.navCtrl.setRoot('HomePage');
     }
   }
   
