af93a07a911c813d075cb7c7a9d49a39b11bc1a6
viktor-lima <joaolimak10@outlook.com>
2022-04-13 21:34:53 -0300

    Tratamento padrao para outros erros com alert

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -2,11 +2,12 @@ import { Injectable } from "@angular/core";
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { StorageService } from "../services/storage.service";
+import { AlertController } from "ionic-angular";
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-    constructor(public storage : StorageService){
+    constructor(public storage : StorageService, public alertCtrl : AlertController){
 
     }
 
@@ -26,16 +27,44 @@ export class ErrorInterceptor implements HttpInterceptor {
                 console.log(errorObj);
 
                 switch (errorObj.status) {
+                    case 401:
+                        this.handle401();
+                        break;
                     case 403:
                         this.handle403
                         break;
                     default:
+                        this.handleDefaultError(errorObj);
                         break;
                 }
-                
-
               return Observable.throw(errorObj);
             }) as any;
+    }
+    handleDefaultError(errorObj) {
+        let alert = this.alertCtrl.create({
+            title: 'Error ' + errorObj.status + ': ' + errorObj.error,
+            message: errorObj.message,
+            enableBackdropDismiss: false,
+            buttons:[
+                {
+                    text: 'ok'
+                }
+            ]
+        });
+        alert.present();
+    }
+    handle401() {
+        let alert = this.alertCtrl.create({
+            title: 'Error 401: Falha de autemticação',
+            message: 'Email ou senha incorretos',
+            enableBackdropDismiss: false,
+            buttons:[
+                {
+                    text: 'ok'
+                }
+            ]
+        });
+        alert.present();
     }
 
     handle403(){
