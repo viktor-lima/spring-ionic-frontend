78f8113d97e6f8d13e9e06ab6d443be226e9aa04
viktor-lima <joaolimak10@outlook.com>
2022-05-05 01:55:12 -0300

    mudanças config.xml

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
-    baseUrl: "http://localhost:8080",
+    baseUrl: "https://vlk-spring-ionic.herokuapp.com",
     bucketBaseUrl: "https://vkl-curso-spring-ionic.s3.sa-east-1.amazonaws.com"
 }
